6dd70ff9e21bd403dc47b184f59546bba1ff2fb5
EL Mahdi HADDOUCH <elmahdi.haddouch@sii-maroc.com>
2025-01-03 22:08:34 +0100

    first commit portfoliov5

--- app/page.tsx
+++ app/page.tsx
@@ -3,25 +3,15 @@
 import dynamic from "next/dynamic";
 import { navItems } from "@/data";
 
-// Importation dynamique des composants
-const Hero = dynamic(() => import("@/components/Hero"), {
+// Importation dynamique des composants avec désactivation explicite du SSR
+const Hero = dynamic(() => import("@/components/Hero"), { ssr: false });
+const FloatingNav = dynamic(() => import("@/components/ui/FloatingNavbar"), {
   ssr: false,
 });
-const FloatingNav = dynamic(
-  () => import("@/components/ui/FloatingNavbar").then((mod) => mod.FloatingNav) // Correction de l'import
-);
-const Grid = dynamic(() => import("@/components/Grid"), {
-  ssr: false,
-});
-const Footer = dynamic(() => import("@/components/Footer"), {
-  ssr: false,
-});
-const Clients = dynamic(() => import("@/components/Clients"), {
-  ssr: false,
-});
-const Approach = dynamic(() => import("@/components/Approach"), {
-  ssr: false,
-});
+const Grid = dynamic(() => import("@/components/Grid"), { ssr: false });
+const Footer = dynamic(() => import("@/components/Footer"), { ssr: false });
+const Clients = dynamic(() => import("@/components/Clients"), { ssr: false });
+const Approach = dynamic(() => import("@/components/Approach"), { ssr: false });
 const Experience = dynamic(() => import("@/components/Experience"), {
   ssr: false,
 });
@@ -33,14 +23,21 @@ const Home = () => {
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
-        {/* Ajout des types corrects pour `FloatingNav` */}
+        {/* Barre de navigation flottante */}
         <FloatingNav navItems={navItems} />
+        {/* Section Héros */}
         <Hero />
+        {/* Section Grille */}
         <Grid />
+        {/* Projets récents */}
         <RecentProjects />
+        {/* Clients */}
         <Clients />
+        {/* Expérience */}
         <Experience />
+        {/* Approche */}
         <Approach />
+        {/* Pied de page */}
         <Footer />
       </div>
     </main>
